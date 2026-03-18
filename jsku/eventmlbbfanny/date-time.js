a7ade8c55bf27af3941efde7057c4d0b897a61bd
TirzzNesia <106883004+TirzzNesia@users.noreply.github.com>
2026-03-18 09:05:46 +0700

    Add files via upload

---
+++ jsku/eventmlbbfanny/date-time.js
@@ -0,0 +1,49 @@
+const displayTime = document.querySelector("#display-time");
+// Time
+function showTime() {
+  let time = new Date();
+  displayTime.innerText = time.toLocaleTimeString("en-US", { hour12: false });
+  setTimeout(showTime, 1000);
+}
+showTime();
+// Date
+function updateDate() {
+  let today = new Date();
+  // return number
+  let dayName = today.getDay(),
+    dayNum = today.getDate(),
+    month = today.getMonth(),
+    year = today.getFullYear();
+
+  const months = [
+    "/01/",
+    "/02/",
+    "/03/",
+    "/04/",
+    "/05/",
+    "/06/",
+    "/07/",
+    "/08/",
+    "/09/",
+    "/10/",
+    "/11/",
+    "/12/",
+  ];
+  const dayWeek = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  // value -> ID of the html element
+  const IDCollection = ["day", "daynum", "month", "year"];
+  // return value array with number as a index
+  const val = [dayWeek[dayName], dayNum, months[month], year];
+  for (let i = 0; i < IDCollection.length; i++) {
+    document.getElementById(IDCollection[i]).firstChild.nodeValue = val[i];
+  }
+}
+updateDate();
