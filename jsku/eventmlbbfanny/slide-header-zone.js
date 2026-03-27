4f96675ebae758b2fe675f5bf2fcd51d88ce3eff
TirzzNesia <106883004+TirzzNesia@users.noreply.github.com>
2026-03-27 13:28:08 +0700

    Update slide-header-zone.js

--- jsku/eventmlbbfanny/slide-header-zone.js
+++ jsku/eventmlbbfanny/slide-header-zone.js
@@ -1,13 +1,11 @@
-let slideIndexHeader = 0;
-showSlidesHeader();
+var slideIndexHeader = 0;
+if (document.getElementsByClassName("sliderHeader").length > 0) showSlidesHeader();
 function showSlidesHeader() {
-    let i;
-    let slidesHeader = document.getElementsByClassName("sliderHeader");
-    for (i = 0; i < slidesHeader.length; i++) {
-        slidesHeader[i].style.display = "none"; 
-    }
+    var i;
+    var slidesHeader = document.getElementsByClassName("sliderHeader");
+    for (i = 0; i < slidesHeader.length; i++) slidesHeader[i].style.display = "none";
     slideIndexHeader++;
-    if (slideIndexHeader > slidesHeader.length) {slideIndexHeader = 1} 
-    slidesHeader[slideIndexHeader-1].style.display = "block"; 
+    if (slideIndexHeader > slidesHeader.length) slideIndexHeader = 1;
+    if (slidesHeader[slideIndexHeader - 1]) slidesHeader[slideIndexHeader - 1].style.display = "block";
     setTimeout(showSlidesHeader, 3000);
-};
+}
