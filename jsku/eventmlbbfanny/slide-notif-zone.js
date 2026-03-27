0df08e26c1c8573b6389d7c50616641e1e5b5c1f
TirzzNesia <106883004+TirzzNesia@users.noreply.github.com>
2026-03-27 13:27:41 +0700

    Update slide-notif-zone.js

--- jsku/eventmlbbfanny/slide-notif-zone.js
+++ jsku/eventmlbbfanny/slide-notif-zone.js
@@ -1,18 +1,14 @@
-let slideIndex = 0;
-showSlides();
-function showSlides() {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
-  }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}    
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" actives", "");
-  }
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " actives";
-  setTimeout(showSlides,6000);
+var slideIndexNotif = 0;
+if (document.getElementsByClassName("mySlides").length > 0) showSlidesNotif();
+function showSlidesNotif() {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    for (i = 0; i < slides.length; i++) slides[i].style.display = "none";
+    slideIndexNotif++;
+    if (slideIndexNotif > slides.length) slideIndexNotif = 1;
+    for (i = 0; i < dots.length; i++) dots[i].className = dots[i].className.replace(" actives", "");
+    if (slides[slideIndexNotif - 1]) slides[slideIndexNotif - 1].style.display = "block";
+    if (dots[slideIndexNotif - 1]) dots[slideIndexNotif - 1].className += " actives";
+    setTimeout(showSlidesNotif, 6000);
 }
